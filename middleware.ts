d43a50ccec0fb9f3388c37ca381cea7c759238ba
Jeel-vadher <jeelvadher0912@gmail.com>
2024-03-27 15:58:24 +0530

    first commit

---
+++ middleware.ts
@@ -0,0 +1,27 @@
+import { authMiddleware } from "@clerk/nextjs";
+ 
+
+ 
+export default authMiddleware({
+  publicRoutes :[
+    '/',
+    'evnets/:id',
+    '/api/webhook/clerk',
+    'api/webhook/stripe',
+    '/api/uploadthing'
+  ],
+  ignoredRoutes: [
+    '/api/webhook/clerk',
+    '/api/webhook/stripe',
+    '/api/uploadthing'
+  ]
+});
+ 
+export const config = {
+  matcher: [
+    
+ 
+    "/((?!.+\\.[\\w]+$|_next).*)",'/',
+    "/(api|trpc)(.*)"
+  ]
+};
